a3679782d03d26a361de629460e2873604acc95b
sho sato <oydgw36@gmail.com>
2024-03-23 17:50:11 +0900

    feat: add example hanler

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,13 +1,15 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/shosato0306/golang-ddd-boilerplate/internal/app/api/handler"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // 0.0.0.0:8080 でサーバーを立てます。
+
+	r.GET("/example", handler.ExampleHandler)
+
+	r.Run()
 }
